cbcf327babda331bb7699da4472d5f4c0d019d41
PraisesPJMT <praisesmusa@outlook.com>
2022-08-30 19:20:06 +0100

    Add event to display comment pop up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles/styles.css';
+import createCommentPop from './module/comment_display_generation.js';
+import getMeals from './module/comment_api_functions.js';
+
+const body = document.querySelector('body');
+
+body.addEventListener('click', (event) => {
+  if (event.target.classList.contains('comment')) {
+    const mealCard = event.target.parentElement.parentElement;
+    const mealId = mealCard.id;
+    getMeals()
+      .then((response) => {
+        const data = response.meals;
+        body.appendChild(createCommentPop(mealId, data));
+      });
+  }
+});
