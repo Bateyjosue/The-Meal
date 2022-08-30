c1880dc9d5a6ac6950df233953a0ffbcb0310dd3
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-08-30 07:21:03 -0700

    Develop Reservation PopUP

---
+++ src/module/reservation.js
@@ -0,0 +1,24 @@
+const showReservations = () => {
+  // create a popup to show Reservations on a particular ID
+  const popup = document.createElement('div');
+  popup.classList.add('popup');
+  popup.innerHTML = `
+    <div class="popup-inner">
+        <div class="popup-header">
+            <h2>Reservations</h2>
+            <span class="material-symbols-outlined">close</span>
+        </div>
+        <div class="popup-body">
+       </div>
+       <article class"popup-footer">
+         <h3> Make a Reservation </h3>
+         <form class="form-inline">
+         <input type="name" class="form-control" placeholder="Your Name"></input>
+         <label for="date">Start Date</label>
+            <input type="date" class="form-control" id="start-date"></input>
+            <label for="date">End Date</label>
+            <input type = "date" class="form-control" id="end-date"></input>
+            <button type = "submit" class="btn btn-primary" id="submitReservation">Reserve!</button>
+
+    `;
+};
