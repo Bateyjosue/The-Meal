aa263f386955f9eb124c9247821a54e3fa81eaee
Bateyjosue <josuebatey19@gmail.com>
2022-08-31 11:50:46 +0200

    Add fetchData likes function to get likes in  an API

--- src/module/data.js
+++ src/module/data.js
@@ -1,7 +1,15 @@
 const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s';
+const appID = '5oMRq53AE4mKSgi6ZQP7';
+const baseUrlLikes = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}`;
 
 const getData = async () => {
   const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+const getLikesData = async () => {
+  const response = await fetch(`${baseUrlLikes}/likes`);
   const data = await response.json();
   return data;
 };
