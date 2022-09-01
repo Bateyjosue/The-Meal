45477dac4844d9945bb45aafbdc26161c405f342
PraisesPJMT <praisesmusa@outlook.com>
2022-09-01 16:44:55 +0100

    Refactor comment names for identical occurances

--- src/module/comment_response_messages.js
+++ src/module/comment_response_messages.js
@@ -3,28 +3,24 @@ export const commentAddSuccess = () => {
     input.value = '';
   });
   const response = document.querySelector('.response-message');
-  response.classList.remove('hide');
-  response.classList.remove('red');
+  response.classList.remove('hide', 'red');
   response.classList.add('green');
   response.innerHTML = 'Comment added successfully!!!';
   setTimeout(() => {
     response.classList.add('hide');
-    response.classList.remove('green');
-    response.classList.remove('red');
+    response.classList.remove('green', 'red');
     response.innerHTML = '';
   }, 5000);
 };
 
 export const commentAddError = () => {
   const response = document.querySelector('.response-message');
-  response.classList.remove('hide');
-  response.classList.remove('green');
+  response.classList.remove('hide', 'green');
   response.classList.add('red');
   response.innerHTML = 'Enter both comment and name.';
   setTimeout(() => {
     response.classList.add('hide');
-    response.classList.remove('green');
-    response.classList.remove('red');
+    response.classList.remove('green', 'red');
     response.innerHTML = '';
   }, 10000);
 };
