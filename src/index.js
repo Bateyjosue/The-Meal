5e61267ffd9941b5d58d853fda62fb23ba141558
Bateyjosue <josuebatey19@gmail.com>
2022-09-01 08:39:48 +0200

    Display likes on items in Home page

--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
 import './styles/styles.css';
 
-import getData from './module/data.js';
+import getData, { postDataLikes, getLikesData } from './module/data.js';
 
 const card = document.querySelector('.list-items .card');
-getData().then((data) => {
-  data.meals.forEach((item) => {
-    card.innerHTML += `
-    <li id="${item.idMeal}">
-          <div class="card-image">
-            <img src="${item.strMealThumb}" alt="${item.strMeal}">
-          </div>
-          <div class="card-title">
-            <span><h2>${item.strMeal}</h2></span>
-            <span class="material-symbols-outlined">favorite</span>
-            <span>5 likes</span>
-          </div>
-          <div class="card-footer">
-            <button type="button" class="comment">Comments<button>
-            <button type="button" id="Reserve">Reservations</button>
-          </div>
-        </li>
-    `;
-  });
+const data = await getData();
+data.meals.forEach((item) => {
+  card.innerHTML += `
+  <li id="${item.idMeal}">
+        <div class="card-image">
+          <img src="${item.strMealThumb}" alt="${item.strMeal}">
+        </div>
+        <div class="card-title">
+          <span><h2>${item.strMeal}</h2></span>
+          <span class="material-symbols-outlined">favorite</span>
+          <span class="like">
+          0 Like
+          </span>
+        </div>
+        <div class="card-footer">
+          <button type="button" class="comment">Comments<button>
+          <button type="button" id="Reserve">Reservations</button>
+        </div>
+      </li>
+  `;
 });
+
+const likes = await getLikesData();
+const span = document.querySelectorAll('.like');
+
+span.forEach((sp) => likes.filter((like) => {
+  if (parseInt(sp.parentNode.parentNode.id, 10) === like.item_id) {
+    sp.innerHTML = `
+      <strong>${like.likes}</strong> Likes
+    `;
+  }
+  return 0;
+}));
