dcd90ba299df9e2b6419bf2fe3b3d42de2642247
Bateyjosue <josuebatey19@gmail.com>
2022-09-01 08:40:27 +0200

    Add function to fetch likes in the API

--- src/module/data.js
+++ src/module/data.js
@@ -1,5 +1,5 @@
 const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s';
-const appID = '5oMRq53AE4mKSgi6ZQP7';
+const appID = 'lrtXrGQjVDOjVbLsfzj1';
 const baseUrlLikes = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}`;
 
 const getData = async () => {
@@ -8,8 +8,22 @@ const getData = async () => {
   return data;
 };
 
-const getLikesData = async () => {
-  const response = await fetch(`${baseUrlLikes}/likes`);
+export const postDataLikes = async (idMeal) => {
+  const like = { 'item_id': idMeal };
+  const set = await fetch(baseUrlLikes,
+    {
+      method: 'POST',
+      body: JSON.stringify(like),
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+    });
+  const result = await set.json();
+  return result;
+};
+
+export const getLikesData = async () => {
+  const response = await fetch(`${baseUrlLikes}/likes/`);
   const data = await response.json();
   return data;
 };
